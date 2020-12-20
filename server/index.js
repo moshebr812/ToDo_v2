b69bddbe4d4f3f579e6365d13163f39ff263411a
Moshe Braude <mbraude@attentigroup.com>
2020-12-20 19:11:20 +0200

    changes in server.index.js ,  project.package.json for Heroku

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const expressSrv = require('express');
 const myApp = expressSrv();
 const { dbConnectViaMongoose } = require('./routers/dbService');
 
+// for Heorku & build server
+const path = require('path');
+
 // expressSrv.Router() ==> Not needed here. Applied in each specific module
 // const routerSrv = expressSrv.Router();  // this now enabled me to catch calls from outside based on the URL I will define on the routerSrv
 
@@ -17,7 +20,7 @@ const todostatusRouter = require ('./routers/Todo/TodoStatus');
 
 
 // Variables & settings
-const port = 3002;
+let port = 3002;
 myApp.set('server version:  s.0.1');        // can define my own variables 
 myApp.set("json spaces", 3);                // so the output is broken to lines and more readable
 
@@ -31,8 +34,13 @@ myApp.use(expressSrv.json());   // convert valid JSON structures to JS Objects
 // middleware       - Set the list of Business Routers as middleware
 myApp.use("/api/todoitems", todoitemsRouter);
 myApp.use("/api/todostatus", todostatusRouter);
+myApp.use("/",expressSrv.static(path.join(__dirname, '../client/build')));
 
 
+if (process.env.PORT) {
+    // Heroku has defined a port for us
+    port = process.env.PORT;
+}
 
 // Start listening
 myApp.listen(port);
